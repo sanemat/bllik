e37000907aa569a4a734d4b7acbc9090538c9dfb
sanemat <o.gata.ken@gmail.com>
2010-05-31 00:57:34 +0900

    add deploy files but must override deploy tasks. todo

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+require 'capistrano/ext/multistage'
+set :application, "bllik"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :default_run_options, :pty => true
+#server "example.com", :web, :app, :db, :primary => true
+#set :deploy_to, "/path/to/#{application}"
+
+set :scm, :git
+set :repository, "git://github.com/sanemat/bllik.git"
+#set :branch, 'foobar'
+set :git_enable_submodules, 1
+set :keep_releases, 3
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
