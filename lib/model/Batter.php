12c6a2ce8545977cd1c47199add2264785e13380
sanemat <o.gata.ken@gmail.com>
2010-06-05 23:41:28 +0900

    add phpdoc comment
    ticket:3

--- lib/model/Batter.php
+++ lib/model/Batter.php
@@ -1,19 +1,39 @@
 <?php
+/**
+ * Batter
+ *
+ */
 class Batter
 {
   private $action;
+
+  /**
+   * setAction
+   *
+   * @param string $action
+   * @return void
+   */
   public function setAction($action)
   {
     $this->action = $action;
   }
-  public function action($action)
-  {
-    $this->action = $action;
-  }
+
+  /**
+   * getAction
+   *
+   * @return string
+   */
   public function getAction()
   {
     return $this->action;
   }
+
+  /**
+   * result
+   *
+   * @param string $result
+   * @return void
+   */
   public function result($result)
   {
   }
--- lib/model/Pitcher.php
+++ lib/model/Pitcher.php
@@ -1,23 +1,59 @@
 <?php
+/**
+ * Pitcher
+ * 
+ */
 class Pitcher
 {
   private $action, $isWin, $fluctuateYen, $result;
+
+  /**
+   * setAction
+   *
+   * @param string $action
+   * @return void
+   */
   public function setAction($action)
   {
     $this->action = $action;
   }
+
+  /**
+   * getAction
+   *
+   * @return string
+   */
   public function getAction()
   {
     return $this->action;
   }
+
+  /**
+   * isWin
+   *
+   * @return bool
+   */
   public function isWin()
   {
     return $this->isWin;
   }
+
+  /**
+   * fluctuateYen
+   *
+   * @return int
+   */
   public function fluctuateYen()
   {
     return $this->fluctuateYen;
   }
+
+  /**
+   * result
+   * 
+   * @param string $result
+   * @return void
+   */
   public function result($result){
     $this->result = $result;
     if($result === 'minogashi'){
@@ -37,6 +73,12 @@ class Pitcher
       $this->fluctuateYen = 2;
     }
   }
+
+  /**
+   * getResult
+   * 
+   * @return string
+   */
   public function getResult()
   {
     return $this->result;
